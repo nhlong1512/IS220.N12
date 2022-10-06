a4afc65ef7f40aad3261f4ababfb8a02f2bb360d
TrongTin <trantrongtin01012002@gmail.com>
2022-10-07 03:06:24 +0700

    dang ky va dang nhap, quen mat khau chua xong

--- Controllers/TrangChuController.cs
+++ Controllers/TrangChuController.cs
@@ -1,20 +1,186 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using MoriiCoffee.Models;
+using WebMatrix.WebData;
 
 namespace MoriiCoffee.Controllers
+  
 {
     public class TrangChuController : Controller
     {
-        private MoriiCoffeeDBContext db = new MoriiCoffeeDBContext();
+        private MoriiCoffeeDBContext _db = new MoriiCoffeeDBContext();
         // GET: TrangChu
         public ActionResult Index()
         {
-            ViewBag.SoMauTin = db.NguoiDungs.Count();
+            if (Session["idUser"] != null)
+            {
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+        }
+        //GET: Register
+
+        public ActionResult Register()
+        {
             return View();
+        }
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(NguoiDung _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = _db.NguoiDungs.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    _db.Configuration.ValidateOnSaveEnabled = false;
+                    _db.NguoiDungs.Add(_user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = _db.NguoiDungs.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().HoTen;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().ID;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+
+
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ForgotPassword(string UserName)
+        {
+            if (ModelState.IsValid)
+            {
+                if (WebSecurity.UserExists(UserName))
+                {
+                    string To = UserName, UserID, Password, SMTPPort, Host;
+                    string token = WebSecurity.GeneratePasswordResetToken(UserName);
+                    if (token == null)
+                    {
+                        // If user does not exist or is not confirmed.
+                        return View("Index");
+                    }
+                    else
+                    {
+                        //Create URL with above token
+                        var lnkHref = "<a href='" + Url.Action("ResetPassword", "Account", new { email = UserName, code = token }, "http") + "'>Reset Password</a>";
+                        //HTML Template for Send email
+                        string subject = "Your changed password";
+                        string body = "<b>Please find the Password Reset Link. </b><br/>" + lnkHref;
+                        //Get and set the AppSettings using configuration manager.
+                        EmailManager.AppSettings(out UserID, out Password, out SMTPPort, out Host);
+                        //Call send email methods.
+                        EmailManager.SendEmail(UserID, subject, body, To, UserID, Password, SMTPPort, Host);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+    public class EmailManager
+    {
+        public static void AppSettings(out string UserID, out string Password, out string SMTPPort, out string Host)
+        {
+            UserID = ConfigurationManager.AppSettings.Get("UserID");
+            Password = ConfigurationManager.AppSettings.Get("Password");
+            SMTPPort = ConfigurationManager.AppSettings.Get("SMTPPort");
+            Host = ConfigurationManager.AppSettings.Get("Host");
+        }
+        public static void SendEmail(string From, string Subject, string Body, string To, string UserID, string Password, string SMTPPort, string Host)
+        {
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.To.Add(To);
+            mail.From = new MailAddress(From);
+            mail.Subject = Subject;
+            mail.Body = Body;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = Host;
+            smtp.Port = Convert.ToInt16(SMTPPort);
+            smtp.Credentials = new NetworkCredential(UserID, Password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
         }
     }
 }
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MoriiCoffee.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace MoriiCoffee.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-7VDCCT1;Initial Catalog=ShopThoiTrang;Integrated Security=Tru" +
-            "e")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=TrongTin;Initial Catalog=DoAn;Integrated Security=True")]
         public string ChuoiKN {
             get {
                 return ((string)(this["ChuoiKN"]));
