43f8f36d7236dbefdc543ad8413890f2ce40da46
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-10 14:54:16 +0700

    Chia route va them giao dien co ban cho login register va forgotpassword

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,11 +13,35 @@ namespace MoriiCoffee
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "DangNhap",
+                url: "dang-nhap",
+                defaults: new { controller = "User", action = "DangNhap", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DangKy",
+                url: "dang-ky",
+                defaults: new { controller = "User", action = "DangKy", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "QuenMatKhau",
+               url: "quen-mat-khau",
+               defaults: new { controller = "User", action = "QuenMatKhau", id = UrlParameter.Optional }
+           );
+
+
+            //Route mặc định luôn phải để ở cuối cùng, tránh ghi đè lên các route đã được customize
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "TrangChu", action = "TrangChu", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoriiCoffee.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        public ActionResult QuenMatKhau()
+        {
+            return View();
+        }
+    }
+}
