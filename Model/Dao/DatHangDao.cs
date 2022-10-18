1516d204e11387498052af10e03d8c6a4ace9ef2
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-18 08:08:52 +0700

    Thêm ViewDetail, Insert, Update, Delete cho DatHangDao

--- Model/Dao/DatHangDao.cs
+++ Model/Dao/DatHangDao.cs
@@ -10,5 +10,61 @@ namespace Model.Dao
 {
     class DatHangDao
     {
+        MoriiCoffeeDBContext db = null;
+
+        public DatHangDao()
+        {
+            db = new MoriiCoffeeDBContext();
+        }
+
+        //Xem chi tiết DatHang
+        public DatHang ViewDetail(int id)
+        {
+            return db.DatHangs.Find(id);
+        }
+
+        //Thêm DatHang
+        public long Insert(DatHang entity)
+        {
+            db.DatHangs.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //Update DatHang
+        public bool Update(DatHang entity)
+        {
+            try
+            {
+                var dh = db.DatHangs.Find(entity.ID);
+                dh.MaHoaDon = entity.MaHoaDon;
+                dh.DiaChiNhanHang = entity.DiaChiNhanHang;
+                dh.UrlImage = entity.UrlImage;
+                dh.ModifiedBy = entity.ModifiedBy;
+                dh.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //Xóa DatHang
+        public bool Delete(int id)
+        {
+            try
+            {
+                var nd = db.DatHangs.Find(id);
+                db.DatHangs.Remove(nd);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
