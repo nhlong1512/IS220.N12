8a546f775941e67b05c5be38ada069d08e355038
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-18 08:06:20 +0700

    Thêm ViewDetail, Insert, Update, Delete cho CuaHangDao

--- Model/Dao/CuaHangDao.cs
+++ Model/Dao/CuaHangDao.cs
@@ -10,5 +10,63 @@ namespace Model.Dao
 {
     class CuaHangDao
     {
+        MoriiCoffeeDBContext db = null;
+
+        public CuaHangDao()
+        {
+            db = new MoriiCoffeeDBContext();
+        }
+
+        //Xem chi tiết CuaHang
+        public CuaHang ViewDetail(int id)
+        {
+            return db.CuaHangs.Find(id);
+        }
+
+        //Thêm CuaHang
+        public long Insert(CuaHang entity)
+        {
+            db.CuaHangs.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //Update CuaHang
+        public bool Update(CuaHang entity)
+        {
+            try
+            {
+                var ch = db.CuaHangs.Find(entity.ID);
+                ch.TenCuaHang = entity.TenCuaHang;
+                ch.SDT = entity.SDT;
+                ch.MaQuanLy = entity.MaQuanLy;
+                ch.DiaChi = entity.DiaChi;
+                ch.UrlImage = entity.UrlImage;
+                ch.ModifiedBy = entity.ModifiedBy;
+                ch.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //Xóa CuaHang
+        public bool Delete(int id)
+        {
+            try
+            {
+                var nd = db.CuaHangs.Find(id);
+                db.CuaHangs.Remove(nd);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
