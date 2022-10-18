58bee71f76b7e2bb031c6e892ca7521a1eaa2a11
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-18 08:11:10 +0700

    Thêm ViewDetail, Insert, Update, Delete cho HoaDonDao

--- Model/Dao/HoaDonDao.cs
+++ Model/Dao/HoaDonDao.cs
@@ -10,5 +10,62 @@ namespace Model.Dao
 {
     class HoaDonDao
     {
+        MoriiCoffeeDBContext db = null;
+
+        public HoaDonDao()
+        {
+            db = new MoriiCoffeeDBContext();
+        }
+
+        //Xem chi tiết HoaDon
+        public HoaDon ViewDetail(int id)
+        {
+            return db.HoaDons.Find(id);
+        }
+
+        //Thêm HoaDon
+        public long Insert(HoaDon entity)
+        {
+            db.HoaDons.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //Update HoaDon
+        public bool Update(HoaDon entity)
+        {
+            try
+            {
+                var hd = db.HoaDons.Find(entity.ID);
+                hd.MaNV = entity.MaNV;
+                hd.MaCH = entity.MaCH;
+                hd.MaKH = entity.MaKH;
+                hd.TongTien = entity.TongTien;
+                hd.ModifiedBy = entity.ModifiedBy;
+                hd.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //Xóa HoaDon
+        public bool Delete(int id)
+        {
+            try
+            {
+                var nd = db.HoaDons.Find(id);
+                db.HoaDons.Remove(nd);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
