eea1dcba438508ed402b12b328e1be5d7200b9a1
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-18 07:55:54 +0700

    Thêm ViewDetail, Insert, Update, Delete cho BlogDao

--- Model/Dao/BlogDao.cs
+++ Model/Dao/BlogDao.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model.EF;
+
 
 namespace Model.Dao
 {
     class BlogDao
     {
+        MoriiCoffeeDBContext db = null;
+
+        public BlogDao()
+        {
+            db = new MoriiCoffeeDBContext();
+        }
+
+        //Xem chi tiết người dùng
+        public Blog ViewDetail(int id)
+        {
+            return db.Blogs.Find(id);
+        }
+
+        //Thêm Người dùng
+        public long Insert(Blog entity)
+        {
+            db.Blogs.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //Update người dùng
+        public bool Update(Blog entity)
+        {
+            try
+            {
+                var bl = db.Blogs.Find(entity.ID);
+                bl.TieuDe = entity.TieuDe;
+                bl.MoTa = entity.MoTa;
+                bl.NoiDung = entity.NoiDung;
+                bl.UrlImage = entity.UrlImage;
+                bl.ModifiedBy = entity.ModifiedBy;
+                bl.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //Xóa người dùng
+        public bool Delete(int id)
+        {
+            try
+            {
+                var nd = db.Blogs.Find(id);
+                db.Blogs.Remove(nd);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
--- Model/Dao/ChiTietHoaDonDao.cs
+++ Model/Dao/ChiTietHoaDonDao.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Model.EF;
 using System.Threading.Tasks;
 
 namespace Model.Dao
