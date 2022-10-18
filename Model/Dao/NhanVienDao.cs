ff80c6203267c90eb53c97ee7cad2fed6cdf1d2a
nhlong1512 <20521569@gm.uit.edu.vn>
2022-10-18 08:16:50 +0700

    Thêm ViewDetail, Update cho NhanVienDao

--- Model/Dao/NhanVienDao.cs
+++ Model/Dao/NhanVienDao.cs
@@ -10,5 +10,35 @@ namespace Model.Dao
 {
     class NhanVienDao
     {
+        MoriiCoffeeDBContext db = null;
+
+        public NhanVienDao()
+        {
+            db = new MoriiCoffeeDBContext();
+        }
+
+        //Xem chi tiết NhanVien
+        public NhanVien ViewDetail(int id)
+        {
+            return db.NhanViens.Find(id);
+        }
+
+
+        //Update NhanVien
+        public bool Update(NhanVien entity)
+        {
+            try
+            {
+                var nv = db.NhanViens.Find(entity.ID);
+                nv.Luong = entity.Luong;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
     }
 }
