d4a2b3100092d89403b21597b2770b1ac68e989b
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-04 15:05:48 +0700

    Config NguoiDungDao

--- Model/Dao/NguoiDungDao.cs
+++ Model/Dao/NguoiDungDao.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model.EF;
+using PagedList;
+
 
 namespace Model.Dao
 {
@@ -16,6 +18,21 @@ namespace Model.Dao
             db = new MoriiCoffeeDBContext();
         }
 
+        //Xem tất cả NguoiDung
+        public List<NguoiDung> ViewAll()
+        {
+            List<NguoiDung> nds = new List<NguoiDung>();
+            if (db.NguoiDungs.Count() == 0)
+            {
+                return nds;
+            }
+            var list = db.NguoiDungs.Where(p => p.ID > 0);
+            //Convert từ IqueryTable sang list
+            nds = new List<NguoiDung>(list);
+            return nds;
+
+        }
+
         //Xem chi tiết người dùng
         public NguoiDung ViewDetail(int id)
         {
@@ -25,6 +42,7 @@ namespace Model.Dao
         //Thêm Người dùng
         public long Insert (NguoiDung entity)
         {
+            entity.CreatedDate = DateTime.Now;
             db.NguoiDungs.Add(entity);
             db.SaveChanges();
             return entity.ID;
@@ -45,8 +63,6 @@ namespace Model.Dao
                 nd.ModifiedDate = DateTime.Now;
                 db.SaveChanges();
                 return true;
-
-
             }
             catch(Exception ex)
             {
@@ -69,5 +85,17 @@ namespace Model.Dao
                 return false;
             }
         }
+
+        //
+        public IEnumerable<NguoiDung> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<NguoiDung> model = db.NguoiDungs;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.HoTen.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
     }
 }
