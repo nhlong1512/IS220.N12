ea20ca568653eb9ff0d68e35f310a26e230625d6
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-06 19:12:33 +0700

    Thêm LayoutController, Controller này để truyền data cần thiết cho layout, đã handle thành công link tới data, việc của mọi người nếu join code thì xem commit này sẽ hiểu

---
+++ Controllers/LayoutClientController.cs
@@ -0,0 +1,20 @@
+﻿using MoriiCoffee.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoriiCoffee.Controllers
+{
+    public class LayoutClientController : Controller
+    {
+        // GET: LayoutClient
+        public ActionResult GetData()
+        {
+            var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+            ViewBag.session = session;
+            return View();
+        }
+    }
+}
