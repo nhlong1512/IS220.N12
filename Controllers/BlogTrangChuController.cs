7b44af868104f1a111e50986603870831afd9ba2
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-08 15:32:56 +0700

    Xử lý xong UserSession cho cửa hàng và liên hệ

--- Controllers/BlogTrangChuController.cs
+++ Controllers/BlogTrangChuController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 namespace MoriiCoffee.Controllers
 {
 
--- Controllers/CuaHangController.cs
+++ Controllers/CuaHangController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Model.Dao;
+using Model.EF;
+using MoriiCoffee.Common;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,8 +13,33 @@ namespace MoriiCoffee.Controllers
     public class CuaHangController : Controller
     {
         // GET: CuaHang
+        private MoriiCoffeeDBContext db = new MoriiCoffeeDBContext();
+        private SanPham sp = new SanPham();
+        private SanPhamDao spdao = new SanPhamDao();
+        private NguoiDungDao nguoidungdao = new NguoiDungDao();
+        private ChiTietSanPham ctsp = new ChiTietSanPham();
+        private ChiTietSanPhamDao ctspdao = new ChiTietSanPhamDao();
+
         public ActionResult Index()
         {
+            if (ModelState.IsValid)
+            {
+                var session = new UserLogin();
+                session = (UserLogin)Session[CommonConstants.USER_SESSION];
+
+                if (!(session is null))
+                {
+                    ViewBag.session = session;
+                    var nd = nguoidungdao.ViewDetailEmail(session.UserName);
+                    ViewBag.ndd = nd;
+                }
+
+
+                ViewBag.sanphams = spdao.ViewAll();
+                ViewBag.ctsps = ctspdao.ViewAll();
+            }
+
+
             return View();
         }
     }
