4d991e4562c9ba0daeb92ae4ae0263def760e825
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-17 16:46:31 +0700

    Post With Ajax Successfully

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -4,6 +4,8 @@ using MoriiCoffee.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -125,10 +127,11 @@ namespace MoriiCoffee.Controllers
             return View(nd);
         }
 
+
         [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult DoiMatKhau(NguoiDung nd)
+        public JsonResult QuenMatKhau(long id, string oldPassword, string newPassword, string confirmNewPassword)
         {
+
             if (ModelState.IsValid)
             {
                 var session = new UserLogin();
@@ -140,20 +143,63 @@ namespace MoriiCoffee.Controllers
                     var ndd = nguoidungdao.ViewDetailEmail(session.UserName);
                     ViewBag.ndd = ndd;
                 }
-
             }
-
-            var isTrue = nguoidungdao.Update(nd);
-            if (isTrue)
+            var msg = "Thất bại";
+            var nd = nguoidungdao.ViewDetail(id);
+            if(nd.Password == GetMD5(oldPassword))
             {
-                return RedirectToAction("DoiMatKhau", "Profile");
+                msg = "Thành công";
             }
-            else
+            return Json(new
             {
-                ModelState.AddModelError("", "Không lưu được vào CSDL");
-                return RedirectToAction("DoiMatKhau", "Profile");
-            }
-
+                status = true,
+                msg = msg
+            });
         }
+
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+        //[HttpPost]
+        //[ValidateInput(false)]
+        //public ActionResult DoiMatKhau(NguoiDung nd)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var session = new UserLogin();
+        //        session = (UserLogin)Session[CommonConstants.USER_SESSION];
+
+        //        if (!(session is null))
+        //        {
+        //            ViewBag.session = session;
+        //            var ndd = nguoidungdao.ViewDetailEmail(session.UserName);
+        //            ViewBag.ndd = ndd;
+        //        }
+
+        //    }
+
+        //    var isTrue = nguoidungdao.Update(nd);
+        //    if (isTrue)
+        //    {
+        //        return RedirectToAction("DoiMatKhau", "Profile");
+        //    }
+        //    else
+        //    {
+        //        ModelState.AddModelError("", "Không lưu được vào CSDL");
+        //        return RedirectToAction("DoiMatKhau", "Profile");
+        //    }
+
+        //}
     }
 }
