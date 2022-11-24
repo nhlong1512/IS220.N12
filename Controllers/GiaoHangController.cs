c610f34eb8af18f5f010205d16f4403df4211264
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-24 18:09:57 +0700

    Delete api.json and add DatGiaoJson

--- Controllers/GiaoHangController.cs
+++ Controllers/GiaoHangController.cs
@@ -43,9 +43,79 @@ namespace MoriiCoffee.Controllers
             return View(list);
         }
 
-        public ActionResult Province()
+
+        //Handle phương thức đặt giao trong phần giao hàng, khi ấn đặt giao và không xảy ra ngoại lệ nào
+        //Các dữ liệu sẽ được đổ hết vào 3 bảng ChiTietHoaDon, HoaDon và Bảng DatHang
+
+        [HttpPost]
+        public JsonResult DatGiaoJson(long id, string hoTen, string sdt, string diaChi, string ghiChu, string pttt)
         {
-            return View();
+
+            var cart = Session[CartSession];
+                var list = (List<CartItem>)cart;
+            //if (cart != null)
+            //{
+                //if (list.Exists(x => x.ChiTietSanPham.ID == id))
+                //{
+                //    var flag = false;
+                //    foreach (var item in list)
+                //    {
+                //        if (item.ChiTietSanPham.ID == id && item.Size == size && item.Topping == topping && item.Gia == gia)
+                //        {
+                //            item.Quantity += 1;
+                //            flag = true;
+                //        }
+                //    }
+                //    if (!flag)
+                //    {
+                //        var item = new CartItem();
+                //        item.ChiTietSanPham = sanpham;
+                //        item.Quantity = 1;
+                //        item.Gia = gia;
+                //        item.Topping = topping;
+                //        item.Size = size;
+                //        list.Add(item);
+                //    }
+                //}
+                //else
+                //{
+                //    var item = new CartItem();
+                //    item.ChiTietSanPham = sanpham;
+                //    item.Quantity = 1;
+                //    item.Gia = gia;
+                //    item.Topping = topping;
+                //    item.Size = size;
+                //    list.Add(item);
+                //}
+                //Session[CartSession] = list;
+            //}
+            //else
+            //{
+            //    //Tạo mới đối tượng CartItem
+            //    var item = new CartItem();
+            //    item.ChiTietSanPham = sanpham;
+            //    item.Quantity = 1;
+            //    item.Gia = gia;
+            //    item.Topping = topping;
+            //    item.Size = size;
+            //    var list = new List<CartItem>();
+            //    list.Add(item);
+            //    //Gán vào Session
+            //    Session[CartSession] = list;
+            //}
+            return Json(new
+            {
+                status = true,
+                listCart = list,
+                id = id,
+                hoTen = hoTen,
+                sdt = sdt,
+                diaChi = diaChi,
+                ghiChu = ghiChu,
+                pttt = pttt,
+                
+            }); 
         }
+
     }
 }
