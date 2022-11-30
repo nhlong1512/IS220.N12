bac65855864ffa774530012345b340f4f81147eb
nhlong1512 <20521569@gm.uit.edu.vn>
2022-11-30 19:52:08 +0700

    Add UI for DanhSachDonDat

--- Areas/Admin/Controllers/BlogController.cs
+++ Areas/Admin/Controllers/BlogController.cs
@@ -18,22 +18,6 @@ namespace MoriiCoffee.Areas.Admin.Controllers
         private Blog bl = new Blog();
         private BlogDao bldao = new BlogDao();
         private NguoiDungDao nguoidungdao = new NguoiDungDao();
-        //public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
-        //{
-        //    var session = (UserLogin)Session[CommonConstants.USER_SESSION];
-        //    if (session == null)
-        //    {
-        //        return Redirect("/dang-nhap");
-        //    }
-        //    //var blogs = bldao.ViewAll();
-        //    //ViewBag.blogs = blogs;
-        //    var nguoidung = nguoidungdao.ViewDetail(1);
-        //    ViewBag.nguoidung = nguoidung;
-        //    var model = bldao.ListAllPaging(searchString, page, pageSize);
-        //    ViewBag.SearchString = searchString;
-        //    return View(model);
-        //}
-
         public ActionResult DanhSachBlog(string searchString, int page = 1, int pageSize = 5)
         {
             var session = (UserLogin)Session[CommonConstants.USER_SESSION];
--- Areas/Admin/Controllers/DonDatController.cs
+++ Areas/Admin/Controllers/DonDatController.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using Model.Dao;
+using Model.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using MoriiCoffee.Controllers;
+using MoriiCoffee.Common;
 
 namespace MoriiCoffee.Areas.Admin.Controllers
 {
     public class DonDatController : Controller
     {
         // GET: Admin/DonDat
+        private MoriiCoffeeDBContext db = new MoriiCoffeeDBContext();
+        private NguoiDungDao nguoidungdao = new NguoiDungDao();
+        private DatHangDao dhdao = new DatHangDao();
+        private HoaDonDao hddao = new HoaDonDao();
+        private ChiTietHoaDonDao cthddao = new ChiTietHoaDonDao();
+        private ChiTietSanPhamDao ctspdao = new ChiTietSanPhamDao();
         public ActionResult DanhSachDonDat()
         {
+            var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+            if (session == null)
+            {
+                return Redirect("/dang-nhap");
+            }
+
+            ViewBag.dathangs = dhdao.ViewAll();
+            ViewBag.hoadons = hddao.ViewAll();
+            ViewBag.nds = nguoidungdao.ViewAll();
             return View();
         }
     }
